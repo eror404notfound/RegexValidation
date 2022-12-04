ee78cacad0004502dec1feb8cdf0136de7f93416
Ankit <ankitcool4370@gmail.com>
2022-12-04 20:39:10 +0530

    Welcome to the RegexValidation

---
+++ RegexValidation2022/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexValidation2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFirstName = "Raj";
+            bool result = RegexValidationCheck.RegexValidate(inputFirstName);
+            Console.WriteLine("\n{0}", result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RegexValidation2022/RegexValidationCheck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexValidation2022
+{
+    internal class RegexValidationCheck
+    {
+        public static string FirstName = "^[A-Z]{1}[a-z]{2}$";
+        public static bool RegexValidate(string inputFirstName)
+        {
+            return Regex.IsMatch(inputFirstName, FirstName);
+        }
+    }
+}
